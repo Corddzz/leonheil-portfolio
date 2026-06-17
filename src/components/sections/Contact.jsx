6f2864a4c073c92d32bfbd49c17cdad68338f01b
LEONHEIL <leozcordz@gmail.com>
2026-06-17 13:52:34 +0800

    chore: update contact section

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import RevealOnScroll from '../RevealOnScroll';
-import emailjs from 'emailjs-com';
+import { useState } from "react";
+import RevealOnScroll from "../RevealOnScroll";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
 
   const handleSubmit = (e) => {
@@ -16,20 +16,22 @@ const Contact = () => {
       .sendForm(
         import.meta.env.VITE_SERVICE_ID,
         import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
         import.meta.env.VITE_PUBLIC_KEY,
         e.target,
       )
       .then((result) => {
-        alert('Message sent');
-        setFormData({ name: '', email: '', message: '' });
+        alert("Message sent");
+        setFormData({ name: "", email: "", message: "" });
       })
-      .catch(() => alert('An error occurred, please try again later.'));
+      .catch(() => alert("An error occurred, please try again later."));
   };
 
   return (
     <section
       id="contact"
-      className="min-h-screen flex items-center justify-center py-20 bg-[#1f284d] w-full">
+      className="min-h-screen flex items-center justify-center py-20 bg-[#1f284d] w-full"
+    >
       <RevealOnScroll>
         <div className="w-auto px-4 sm:w-[900px]">
           <h2 className="text-3xl font-bold mb-8 text-[#ef4442] text-center">
@@ -83,7 +85,8 @@ const Contact = () => {
             <div className="flex items-center justify-center p-3 my-3">
               <button
                 type="submit"
-                className="sm:max-w-[639px] bg-[#ef4442] text-[#eff8f7] py-3 px-6 rounded font-medium transition relative overflow-hidden hover:translate-y-0.5 hover:shadow-[#ef4442]/50 hover:shadow-lg">
+                className="sm:max-w-[639px] bg-[#ef4442] text-[#eff8f7] py-3 px-6 rounded font-medium transition relative overflow-hidden hover:translate-y-0.5 hover:shadow-[#ef4442]/50 hover:shadow-lg"
+              >
                 Send Message
               </button>
             </div>
