2c9a9e64cfadea0a424bf45b3bde124c2620befe
Leonheil Cordero <leozcordz@gmail.com>
2026-06-28 11:26:18 +0800

    feat: enhance Navbar styling and layout for improved user experience

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="border-b border-neutral-700 h-20 text-white p-5 flex justify-between items-center">
-      <h1>
-        Leonheil <span className="text-teal-500 ">.</span>
-      </h1>
+    <div className="border-b border-neutral-700 h-20 text-white p-10 flex justify-between items-center">
+      <div className="tracking-wider flex items-center gap-2">
+        <a
+          href="#"
+          className="text-2xl font-bold hover:text-teal-300 transition-all duration-300 ease-in-out font-mono"
+        >
+          Leonheil{" "}
+        </a>
 
-      <div className="flex gap-5 text-gray-400 font-mono tracking-wider">
+        <span class="relative flex size-3 mt-0.5">
+          <span class="absolute inline-flex h-full w-full animate-bounce rounded-full bg-teal-400"></span>
+        </span>
+      </div>
+
+      <div className="flex gap-7 text-gray-400 font-mono tracking-wider">
         <a
           href="#"
           className="hover:text-teal-500 transition-all duration-300 ease-in-out"
@@ -29,7 +38,7 @@ const Navbar = () => {
       </div>
 
       <div>
-        <button className="text-teal-500 border rounded-lg p-2 transition-all duration-300 ease-in-out hover:bg-teal-600 hover:text-white hover:-translate-y-0.5 active:translate-y-0">
+        <button className="text-teal-500 border rounded-lg p-2 transition-all duration-300 ease-in-out hover:bg-teal-500/30 hover:text-white hover:-translate-y-0.5 active:translate-y-0 hover:border-teal-500 cursor-pointer">
           Get in touch
         </button>
       </div>
